8c18b0b7cb598e9d417b895497d512aa81087c2b
Andreas <andrikos02@outlook.com>
2023-11-23 00:04:42 +0200

    Added shortcuts.xml file Added Get thing app action using intents Show the result as a popup

--- app/src/main/java/com/github/mrmitew/bankapp/features/main/ui/MainActivity.kt
+++ app/src/main/java/com/github/mrmitew/bankapp/features/main/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.github.mrmitew.bankapp.features.main.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -9,6 +10,10 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.github.mrmitew.bankapp.R
 import com.google.android.material.navigation.NavigationView
+import android.widget.Toast
+import com.github.mrmitew.bankapp.features.accounts.dto.AccountDTO
+import com.github.mrmitew.bankapp.features.backend.internal.FakeBackendImpl
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -40,8 +45,72 @@ class MainActivity : AppCompatActivity() {
             false
         }
 
-//        setSupportActionBar(toolbar)
+        //Handle the incoming intent
+        handleIntent(intent)
     }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        // Handle new incoming intents, if any
+        handleIntent(intent)
+    }
+
+
+//Working sample
+//    private fun handleIntent(intent: Intent?) {
+//        if (intent?.action == Intent.ACTION_VIEW) {
+//            val thingName : String? = intent.getStringExtra("thing.name")
+//
+//            if (thingName != null) {
+//
+//                showToast("AccName: $thingName")
+//            } else {
+//
+//                showToast("Thing name is null")
+//            }
+//        } else {
+//            showDefaultView()
+//        }
+//    }
+
+
+
+    //testing
+    private fun handleIntent(intent: Intent?) {
+
+
+        // Create an instance of FakeBackendImpl
+        val fakeBackendImpl = FakeBackendImpl()
+        if (intent?.action == Intent.ACTION_VIEW) {
+            val accountName : String? = intent.getStringExtra("name")
+            //val accountName = thingName
+            val accountBalance = accountName?.let { fakeBackendImpl.getAccountBalanceByName(it) }
+
+
+
+
+            if (accountBalance != null) {
+                // Display the account balance
+                showToast("Account Balance for $accountName: $accountBalance")
+
+            } else {
+                showToast("No account found with the name: $accountName")
+            }
+        } else {
+            showDefaultView()
+        }
+    }
+
+    private fun showDefaultView() {
+        // default view
+    }
+
+    //show toast for checking
+    private fun showToast(message: String) {
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
 
     override fun onSupportNavigateUp() = findNavController(R.id.main_content).navigateUp()
 }
